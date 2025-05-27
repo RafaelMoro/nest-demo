5c4e836c626125fb593317a59c17945d18f3d3ab
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-26 19:26:49 -0600

    add videogames module and add config for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { VideogamesModule } from './videogames/videogames.module';
+import config from './config';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        CLUSTER: Joi.string().required(),
+        MONGO_USER: Joi.string().required(),
+        MONGO_PWD: Joi.string().required(),
+        MONGO_DB_NAME: Joi.string().required(),
+        MONGO_CONNECTION: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+    VideogamesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
