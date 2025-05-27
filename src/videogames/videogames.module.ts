3078192a811dc13f532990a937b730f54bfb885b
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-26 19:29:00 -0600

    add videogames service

--- src/videogames/videogames.module.ts
+++ src/videogames/videogames.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VideogamesController } from './controller/videogames.controller';
+import { VideogamesService } from './services/videogames.service';
 
 @Module({
   controllers: [VideogamesController],
+  providers: [VideogamesService],
 })
 export class VideogamesModule {}
