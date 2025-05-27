c308f5fea607d8bd299408a5b6d76a438c226efa
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-27 11:12:37 -0600

    update videogame model of the id being string

--- src/videogames/videogame.model.ts
+++ src/videogames/videogame.model.ts
@@ -3,7 +3,7 @@ import { Field, ObjectType, ID } from '@nestjs/graphql';
 @ObjectType({ description: 'Videogame model' })
 export class VideogameGqlModel {
   @Field((type) => ID)
-  _id: number;
+  _id: string;
 
   @Field({ description: 'Videogame name' })
   name: string;
