4b51ad2d97652fb48773254da8abbb25bed9a24c
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-27 12:29:29 -0600

    add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Demo API')
+    .setDescription(
+      'This API is connected to mongoDB and uses graphQL and REST',
+    )
+    .setVersion('1.0')
+    .addTag('videogames')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
