176fc977fe898c24e34fbeab19a398b85e586699
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-26 19:26:58 -0600

    add db module

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,27 @@
-import { Module } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Module, Global } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigType } from '@nestjs/config';
+import config from '../config';
 
-@Module({})
+@Global()
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { connection, user, password, cluster, mongoDbName } =
+          configService.database;
+        return {
+          uri: `${connection}://${user}:${password}@${cluster}.q040o.mongodb.net/?retryWrites=true&w=majority`,
+          user,
+          pass: password,
+          dbName: mongoDbName,
+        };
+      },
+      inject: [config.KEY],
+    }),
+  ],
+})
 export class DatabaseModule {}
