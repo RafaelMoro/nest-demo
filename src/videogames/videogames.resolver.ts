cc6b76f0ab298cd7b2516bf816c639729e978843
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-05-26 21:57:39 -0600

    add resolver to return single videogame

--- src/videogames/videogames.resolver.ts
+++ src/videogames/videogames.resolver.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { VideogameGqlModel } from './videogame.model';
 import { VideogamesService } from './services/videogames.service';
 
-@Resolver(() => VideogameGqlModel)
+@Resolver((of) => VideogameGqlModel)
 export class VideogameResolvers {
   constructor(private videogameServide: VideogamesService) {}
 
@@ -12,6 +12,12 @@ export class VideogameResolvers {
   async videogames() {
     const data = await this.videogameServide.findAllVideogames();
     return data;
+  }
+
+  @Query((returns) => VideogameGqlModel, { name: 'videogame', nullable: true })
+  async getVideogame(@Args('name') name: string) {
+    const videogame = await this.videogameServide.getSingleVideogame(name);
+    return videogame;
   }
 
   // @ResolveField()
