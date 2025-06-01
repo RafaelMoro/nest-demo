42d6898c420a68daff730425ce0ea54b077d9697
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-01 17:46:49 -0600

    update guard

--- src/auth/guards/jwt-guard/jwt-guard.guard.ts
+++ src/auth/guards/jwt-guard/jwt-guard.guard.ts
@@ -1,11 +1,34 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { ExecutionContext, Injectable, Inject } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { ConfigType } from '@nestjs/config';
+
+import { JWT_STRATEGY } from '@/constants';
+import config from '@/config';
 
 @Injectable()
-export class JwtGuardGuard implements CanActivate {
+export class JwtGuardGuard extends AuthGuard(JWT_STRATEGY) {
+  constructor(
+    private reflector: Reflector,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {
+    super();
+  }
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const isPublic = this.reflector.get(
+      this.configService.auth.publicKey,
+      context.getHandler(),
+    );
+    if (isPublic) return true;
+
+    return super.canActivate(context);
   }
 }
