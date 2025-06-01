31620c1849f8a5b6d7921677efebf5410afdf594
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-01 15:24:40 -0600

    fix unit test videogame service

--- src/videogames/services/videogames.service.spec.ts
+++ src/videogames/services/videogames.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VideogamesService } from './videogames.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Videogame } from '../entities/videogames.entity';
 
 describe('VideogamesService', () => {
   let service: VideogamesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VideogamesService],
+      providers: [
+        VideogamesService,
+        { provide: getModelToken(Videogame.name), useValue: jest.fn() },
+      ],
     }).compile();
 
     service = module.get<VideogamesService>(VideogamesService);
