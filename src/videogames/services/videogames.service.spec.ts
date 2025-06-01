8826c72634eb8a2b304d9166b753371b0d65158b
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-01 16:43:07 -0600

    add UT for get single videogame service

--- src/videogames/services/videogames.service.spec.ts
+++ src/videogames/services/videogames.service.spec.ts
@@ -35,4 +35,21 @@ describe('VideogamesService', () => {
     const response = await service.findAllVideogames();
     expect(response).toBe(result);
   });
+
+  it('getSingleVideogame service should return a videogame', async () => {
+    const result = {
+      _id: '68351afb0e685e9fe702e63b',
+      name: 'The last of us',
+      description: 'A thriller videogame with zombies and a lot of action',
+      price: 1200,
+      platform: ['xbox series x', 'playstatiuon 5'],
+    } as unknown as VideogameDoc;
+
+    jest
+      .spyOn(service, 'getSingleVideogame')
+      // eslint-disable-next-line @typescript-eslint/require-await
+      .mockImplementation(async () => result);
+    const response = await service.getSingleVideogame('The last of us');
+    expect(response).toBe(result);
+  });
 });
