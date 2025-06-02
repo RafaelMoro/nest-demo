18131714196d193bf6346f69b1b7c89912119831
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-01 18:08:37 -0600

    add service findByEmail

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from '../entities/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async findByEmail(email: string) {
+    try {
+      return this.userModel.findOne({ email }).exec();
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+}
