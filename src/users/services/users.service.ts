59d4102c43e0079f3ef15c2b9184be026d28cd29
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-01 18:25:21 -0600

    add create user service

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,16 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from '../entities/users.entity';
+import * as bcrypt from 'bcryptjs';
+
+import { User, UserDoc } from '../entities/users.entity';
+import { CreateUserDto } from '../dtos/users.dto';
+import { USER_EXISTS_ERROR } from '@/constants';
+import { CreateUserResponse } from '../users.interface';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async findByEmail(email: string) {
+  async findByEmail(email: string): Promise<UserDoc | null> {
     try {
       return this.userModel.findOne({ email }).exec();
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async createUser(data: CreateUserDto): Promise<CreateUserResponse> {
+    try {
+      //Verify if the user exists with the same email.
+      const { email: emailData } = data;
+      const user: UserDoc | null = await this.findByEmail(emailData);
+      if (user) throw new BadRequestException(USER_EXISTS_ERROR);
+
+      const userModel = new this.userModel(data);
+      const passwordHashed = await bcrypt.hash(userModel.password, 10);
+      userModel.password = passwordHashed;
+      const modelSaved: UserDoc = await userModel.save();
+      const responseCreateUser = modelSaved.toJSON();
+      const { email, _id: sub } = responseCreateUser;
+      const response: CreateUserResponse = {
+        email,
+        sub,
+      };
+      return response;
     } catch (error) {
       throw new BadRequestException(error.message);
     }
