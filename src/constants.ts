60a4beeec35de6a77de23330cd5a6be52861c279
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-01 18:20:36 -0600

    add more constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,9 @@
 export const LOCAL_STRATEGY = 'local';
 export const JWT_STRATEGY = 'jwt';
 export const JWT_EXPIRE_TIME = '5d';
+
+export const VERSION_RESPONSE = process.env.npm_package_version;
+
+/** Messages */
+export const USER_EXISTS_ERROR = 'Try with other email.';
+export const USER_CREATED_MESSAGE = 'User created';
