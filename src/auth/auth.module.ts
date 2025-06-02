82f3af0cfcd112ac5244c9df76bebefd4b8499b3
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-01 18:52:22 -0600

    add auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './controller/auth.controller';
 
 @Module({
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
