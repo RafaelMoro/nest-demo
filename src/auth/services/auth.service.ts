02bdfa9599267ff4b227520455bb1f594e352bef
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-01 18:34:50 -0600

    add service of validate password of user in auth service

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+
+import { UsersService } from '@/users/services/users.service';
+import { UserDoc } from '@/users/entities/users.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validatePasswordOfUser(email: string, password: string) {
+    const user: UserDoc | null = await this.usersService.findByEmail(email);
+    if (!user) return null;
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...rta } = user.toJSON();
+      return rta;
+    }
+    return null;
+  }
+}
