937e055f65cb6c7e939e126711b21e852608391a
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-03 12:10:50 -0600

    integrate profile interface into validate fn

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { Injectable, Inject, BadRequestException } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
@@ -9,6 +9,7 @@ import { GOOGLE_STRATEGY } from '@/constants';
 import config from '@/config';
 import { createRandomString, getGoogleCallbackUri } from '../auth.utils';
 import { AuthService } from '../services/auth.service';
+import { GoogleProfile } from '@/users/users.interface';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(
@@ -30,17 +31,18 @@ export class GoogleStrategy extends PassportStrategy(
   async validate(
     accessToken: string,
     refreshToken: string,
-    profile: any,
+    profile: GoogleProfile,
     done: VerifyCallback,
   ) {
     try {
       // Do not use the accessToken and refreshToken gotten from Google, we have to create our own.
       const { name, emails } = profile;
+      const [email] = emails;
       const tempPassword = createRandomString(20);
       const currentUser = await this.authService.validateGoogleUser({
-        email: emails[0].value as string,
-        firstName: name.givenName as string,
-        lastName: name.familyName as string,
+        email: email.value,
+        firstName: name.givenName,
+        lastName: name.familyName,
         password: tempPassword, // Google OAuth does not use a password
         hasGoogleLogin: true,
       });
