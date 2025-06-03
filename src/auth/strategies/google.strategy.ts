7556553bf338930a707bdc277c47780a3aed3658
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-03 03:03:15 -0600

    use google validate user in validate function in google strategy

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -8,13 +8,17 @@ import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { GOOGLE_STRATEGY } from '@/constants';
 import config from '@/config';
 import { getGoogleCallbackUri } from '../auth.utils';
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(
   Strategy,
   GOOGLE_STRATEGY,
 ) {
-  constructor(@Inject(config.KEY) configService: ConfigType<typeof config>) {
+  constructor(
+    private authService: AuthService,
+    @Inject(config.KEY) configService: ConfigType<typeof config>,
+  ) {
     super({
       clientID: configService.auth.googleClient ?? '',
       clientSecret: configService.auth.googleClientSecret ?? '',
@@ -29,17 +33,15 @@ export class GoogleStrategy extends PassportStrategy(
     profile: any,
     done: VerifyCallback,
   ) {
-    console.log({ profile });
-    const { name, emails, photos } = profile;
-    const user = {
-      email: emails[0].value,
-      firstName: name.givenName,
-      lastName: name.familyName,
-      picture: photos[0].value,
-      accessToken,
-      refreshToken,
-    };
-
-    done(null, user);
+    // Do not use the accessToken and refreshToken gotten from Google, we have to create our own.
+    const { name, emails } = profile;
+    const currentUser = await this.authService.validateGoogleUser({
+      email: emails[0].value as string,
+      firstName: name.givenName as string,
+      middleName: '',
+      lastName: name.familyName as string,
+      password: '', // Google OAuth does not use a password
+    });
+    done(null, currentUser);
   }
 }
