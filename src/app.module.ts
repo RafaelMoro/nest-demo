2a95ad6b86e6537e7bf3156cea92bda9230fde48
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-02 22:40:31 -0600

    add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { join } from 'node:path';
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
+import { LoggedMiddleware } from './middlewares/LoggedMiddleware.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
@@ -43,4 +44,8 @@ import config from './config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggedMiddleware).forRoutes('*');
+  }
+}
