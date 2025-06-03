74222a84fd4594b78392784993a95f6225a2a090
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-02 23:11:03 -0600

    remove eslint supression

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -8,14 +8,12 @@ import { PayloadToken } from '../auth.interface';
 import config from '@/config';
 
 @Injectable()
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 export class JwtStrategy extends PassportStrategy(Strategy, JWT_STRATEGY) {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
   constructor(@Inject(config.KEY) configService: ConfigType<typeof config>) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+
       secretOrKey: configService.auth.jwtKey ?? '',
     });
   }
