cced399bcb2318bf4237f1f39b6b2f76d18a8e3c
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-03 03:02:33 -0600

    add google validate user auth service

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -6,6 +6,7 @@ import { UsersService } from '@/users/services/users.service';
 import { User, UserDoc } from '@/users/entities/users.entity';
 import { generateJWT } from '../auth.utils';
 import { LoginData } from '@/users/users.interface';
+import { CreateUserDto } from '@/users/dtos/users.dto';
 
 @Injectable()
 export class AuthService {
@@ -34,6 +35,20 @@ export class AuthService {
       user,
     };
     return loginData;
+  }
+
+  async validateGoogleUser(googleUser: CreateUserDto) {
+    try {
+      const userFound = await this.usersService.findByEmail(googleUser.email);
+      if (userFound) return userFound;
+      return await this.usersService.createUser(googleUser);
+    } catch (error) {
+      throw new NotFoundException(
+        'Error validating Google user',
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+        error.message,
+      );
+    }
   }
 
   googleLogin(user: Express.User | undefined) {
