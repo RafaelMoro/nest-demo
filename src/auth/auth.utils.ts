0402ad8088356b5e8234022e10ee322613bfee5a
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-03 12:00:28 -0600

    create fn to create random string

--- src/auth/auth.utils.ts
+++ src/auth/auth.utils.ts
@@ -32,3 +32,14 @@ export const getGoogleCallbackUri = (
   }
   return `${feUri}/auth/google-redirect`;
 };
+
+export function createRandomString(length: number): string {
+  let result = '';
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
