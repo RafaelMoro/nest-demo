fba3e56c368cade5e58b91810563d736c9d91644
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-03 02:37:00 -0600

    add auth service google login

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
 
@@ -34,5 +34,15 @@ export class AuthService {
       user,
     };
     return loginData;
+  }
+
+  googleLogin(user: Express.User | undefined) {
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return {
+      message: 'User information from google',
+      user,
+    };
   }
 }
