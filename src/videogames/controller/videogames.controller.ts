de1eaa3f5ff061be747c88aaa7acdd71c076533f
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-06-05 01:15:31 -0600

    add endpoint protected by role

--- src/videogames/controller/videogames.controller.ts
+++ src/videogames/controller/videogames.controller.ts
@@ -13,6 +13,8 @@ import { Public } from '@/auth/decorators/public/public.decorator';
 import { JwtGuard } from '@/auth/guards/jwt-guard/jwt-guard.guard';
 import { VideogamesService } from '../services/videogames.service';
 import { CreateVideogameDto, UpdateVideogameDto } from '../dtos/videogames.dto';
+import { Roles } from '@/auth/decorators/roles/roles.decorator';
+import { RolesGuard } from '@/auth/guards/roles/roles.guard';
 
 @UseGuards(JwtGuard)
 @Controller('videogames')
@@ -22,6 +24,13 @@ export class VideogamesController {
   @Get()
   @Public()
   async getAllVideogames() {
+    return this.videogameService.findAllVideogames();
+  }
+
+  @Roles('admin', 'editor')
+  @UseGuards(RolesGuard)
+  @Get('protected')
+  async getAllVideogamesWithRole() {
     return this.videogameService.findAllVideogames();
   }
 
